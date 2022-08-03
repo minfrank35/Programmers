f1c74cb65b1f51ebc672433d388393d5d6962e68
minfrank35 <minfrank35@gmail.com>
2022-08-03 22:44:58 +0900

    평균구하기

--- CODE_TEST_PROGRAMMERS/src/beginner/Average.java
+++ CODE_TEST_PROGRAMMERS/src/beginner/Average.java
@@ -2,5 +2,11 @@ package beginner;
 
 //Æò±Õ±¸ÇÏ±â
 public class Average {
-
+	public double solution(int[] arr) {
+        double answer = 0;
+        for(int i : arr) {
+        	answer += i;
+        }
+        return answer/arr.length;
+    }
 }
