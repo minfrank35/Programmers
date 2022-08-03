fc75c958ad3da5bf2d2181ed41f4ebdddeb8822b
minfrank35 <minfrank35@gmail.com>
2022-08-04 00:05:46 +0900

    p부터 y 까지

--- CODE_TEST_PROGRAMMERS/src/beginner/PToTheY.java
+++ CODE_TEST_PROGRAMMERS/src/beginner/PToTheY.java
@@ -1,5 +1,23 @@
 package beginner;
 
 public class PToTheY {
+	boolean solution(String s) {
+        boolean answer = false;
+        int countY = 0;
+        int countP = 0;
+        s = s.toLowerCase();
+        for(int i = 0; i < s.length(); i++) {
+        	if(s.charAt(i) == 'y') {
+        		countY++;
+        	} else if(s.charAt(i) =='p') {
+        		countP++;
+        	} 
+        }
+        
+        if(countY == countP) {
+        	answer = true;
+        }
 
+        return answer;
+    }
 }
