895205c889cb49bff4d65950b990ab02d7a6cb8d
minfrank35 <minfrank35@gmail.com>
2022-08-03 22:58:19 +0900

    문자열 다루기 기본

--- CODE_TEST_PROGRAMMERS/src/beginner/BasicStringTreat.java
+++ CODE_TEST_PROGRAMMERS/src/beginner/BasicStringTreat.java
@@ -1,9 +1,20 @@
 package beginner;
 
+//문자열 다루기 기본
 public class BasicStringTreat {
     public boolean solution(String s) {
         boolean answer = true;
+        if(s.length() == 4 || s.length() == 6) {
+        	for(int i = 0; i < s.length(); i++ ) {
+        		if(!Character.isDigit(s.charAt(i))) {
+        			answer = false;
+        			break;
+        		}
+        	}
+        } else {
+        	answer = false;
+        }
+        
         return answer;
     }
-
 }
